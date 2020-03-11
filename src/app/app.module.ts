7f9ef91a529caf690bc47db3c20b63cecb17827f
emanuelvirca@gmail.com <emanuelvirca@gmail.com>
2020-03-11 12:29:15 +0200

    chore: master commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { Level4Component } from "./app/components/level1/level2/level3/level4/level4.component";
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './reducers';
-import { EffectsModule } from '@ngrx/effects';
-import { AppEffects } from './app.effects';
+import { StoreModule } from "@ngrx/store";
+import { reducers, metaReducers } from "./reducers";
+import { EffectsModule } from "@ngrx/effects";
+import { AppEffects } from "./app.effects";
 
 @NgModule({
-  declarations: [AppComponent, Level4Component],
-  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, Level4Component],
+    imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), EffectsModule.forRoot([AppEffects])],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+    constructor() {}
+}
